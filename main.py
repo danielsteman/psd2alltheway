29493a8b2af50f3dae2a09bfd25f13e7be53c312
Daniel Steman <daniel-steman@live.nl>
2023-12-09 15:21:36 +0100

    idk

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ AUTH = "https://oauth-sandbox.rabobank.nl/openapi/sandbox/oauth2-premium/authori
 
 params = {
     "client_id": os.environ["CLIENT_ID"],
+    "client_secret": os.environ["CLIENT_SECRET"],
     "scope": "ais.balances.read",
     "response_type": "code",
+    "redirect_uri": "http://localhost:8000/redirect",
 }
 
 with open("cert.pem", "rb") as cert_file:
